809be3116eb5ca82f95ba6e68abef5b5c9a4f8be
niakelle <morak0398@gmail.com>
2023-02-05 14:31:33 +0200

    styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,63 @@
+import "./css/reset.css";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>База знань Реакт</h1>
-        <div>Пошук</div>
+    <div className="app">
+      <header className="app-header">
+        <h1 className="app-header__heading">База знань Реакт</h1>
+        <div className="app-header__search">Пошук</div>
       </header>
-      <main>
-        <aside>
-          
+      <main className="app-main">
+        <aside className="sidebar">
+          <nav>
+            <ul>
+              <li>
+                <a href="#">JSX</a>
+              </li>
+              <li>
+                <a href="#">State</a>
+              </li>
+              <li>
+                <a href="#">Props</a>
+              </li>
+            </ul>
+          </nav>
         </aside>
+
+        <article className="article">
+          <h2>JSX</h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci,
+            nihil? Eveniet optio quia temporibus non minus error sapiente natus
+            aliquam excepturi rem assumenda quaerat itaque at ad, ipsa
+            dignissimos aspernatur.
+          </p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci,
+            nihil? Eveniet optio quia temporibus non minus error sapiente natus
+            aliquam excepturi rem assumenda quaerat itaque at ad, ipsa
+            dignissimos aspernatur.
+          </p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci,
+            nihil? Eveniet optio quia temporibus non minus error sapiente natus
+            aliquam excepturi rem assumenda quaerat itaque at ad, ipsa
+            dignissimos aspernatur.
+          </p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci,
+            nihil? Eveniet optio quia temporibus non minus error sapiente natus
+            aliquam excepturi rem assumenda quaerat itaque at ad, ipsa
+            dignissimos aspernatur.
+          </p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci,
+            nihil? Eveniet optio quia temporibus non minus error sapiente natus
+            aliquam excepturi rem assumenda quaerat itaque at ad, ipsa
+            dignissimos aspernatur.
+          </p>
+        </article>
       </main>
     </div>
   );
